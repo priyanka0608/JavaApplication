9d3a4d1d80a2e79fbeb8bf68507711c51b2589d5
priyanka0608 <priyanka_gupta383@yahoo.com>
2014-12-30 08:19:29 -0800

    First Git Project
    Git Project

---
+++ src/First.java
@@ -0,0 +1,12 @@
+import java.util.*; 
+  class Explorer2 { 
+ public static void main(String[] args)
+ { 
+	 
+	 
+	 short s=0;
+	 int x=07;
+	 System.out.println("My first GIT project");
+	 
+ }
+  }
