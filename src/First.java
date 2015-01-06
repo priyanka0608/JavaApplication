c28ffe32633c60fba90e7b3d5ee86db9361980a5
priyanka0608 <priyanka_gupta383@yahoo.com>
2015-01-06 09:42:10 -0800

    again using kdiff3

--- src/First.java
+++ src/First.java
@@ -3,13 +3,12 @@ import java.util.*;
  public static void main(String[] args)
  { 
 	 
-
-	 
-	 short s=0;
-	 int x=07;
-	 System.out.println("My first GIT project. Checking out git commands");
-
-	 System.out.println("My first GIT project");	
-	 System.out.println("Chking mergetool");
+System.out.println("hello world");
+callMe();
+ }
+ 
+ public static void callMe() {
+	 System.out.println("You call me...So swt!!!! I will be coming");
  }
   }
+  
